174fae1699cb30a9fa1ffb2a090c740a6818717f
Camilo <camilo.acosta97@gmail.com>
2024-08-20 06:50:40 -0500

    refactor: Update login and register controllers to handle CORS headers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,36 +1,37 @@
-import User from "../models/user.js";
-import bcrypt from "bcryptjs";
+import User from '../models/user.js'
+import bcrypt from 'bcryptjs'
 
 export const loginUser = async (req, res) => {
-  const { email, password } = req.body.email;
+  res.header('Access-Control-Allow-Origin', '*')
+  const { email, password } = req.body.email
 
   try {
     // Buscar usuario por email
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
 
     if (!user) {
-      return res.status(400).json({ message: "Credenciales inválidas" });
+      return res.status(400).json({ message: 'Credenciales inválidas' })
     }
 
     // Verificar la password
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
 
     if (!isMatch) {
-      return res.status(400).json({ message: "Credenciales inválidas" });
+      return res.status(400).json({ message: 'Credenciales inválidas' })
     }
 
     // const token = generateToken(user._id)
 
     res.status(200).json({
-      message: "Login exitoso",
+      message: 'Login exitoso',
       user: {
         id: user._id,
         email: user.email,
         name: user.name,
-        lastName: user.lastName,
-      },
-    });
+        lastName: user.lastName
+      }
+    })
   } catch (error) {
-    res.status(500).json({ message: "Error en el login", error });
+    res.status(500).json({ message: 'Error en el login', error })
   }
-};
+}
